3b13f5d60323b8824371e631e120d9f8fcf10073
JinWooP98 <qwdk0406@naver.com>
2024-06-18 15:33:38 +0900

    # 스케줄 mapper 및 xml 생성

---
+++ src/main/java/com/project/schedules/dto/ScheduleFindAllDto.java
@@ -0,0 +1,21 @@
+package com.project.schedules.dto;
+
+import com.project.schedules.entity.Schedules;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ScheduleFindAllDto {
+    private long scheduleNo;
+    private String scheduleTitle;
+    private String scheduleImage;
+    private int scheduleViewCount;
+    private int scheduleCount;
+    private LocalDateTime scheduleAt;
+}
