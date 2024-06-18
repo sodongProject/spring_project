48e8e36a38408abb27aa331b1ff942ad090cb4dd
hyerin11 <hodolin24@naver.com>
2024-06-18 15:35:06 +0900

    # gender, auth 엔터티

--- src/main/java/com/project/schedules/entity/Gender.java
+++ src/main/java/com/project/schedules/entity/Gender.java
@@ -1,5 +1,12 @@
 package com.project.schedules.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
 public enum Gender {
     M, F
 }
