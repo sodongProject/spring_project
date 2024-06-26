967eae9fd9ed145e8640ec8d99a1e88df03409ab
munjieun <mun.mimi925@gmail.com>
2024-06-26 13:53:03 +0900

    myPage controller 수정

--- src/main/java/com/project/myPage/controller/MyPageController.java
+++ src/main/java/com/project/myPage/controller/MyPageController.java
@@ -13,10 +13,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Controller
